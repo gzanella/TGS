50348c599fc2d2d3ee8e9e22fd22b06112aaa9d6
BIDSA-STC3-02\ZanellaG <zanella.gcm@gmail.com>
2018-05-07 11:21:27 +0200

    minor changes. loading files from raw.github rather than local directory

--- Example_with_simulated_data.R
+++ Example_with_simulated_data.R
@@ -1,21 +1,21 @@
-setwd('C:/Users/ZanellaG/Bitbucket/TGS/')
-source('functions_for_BVS.R')
+### THE FOLLOWING COMMAND SOURCES THE R CODE WITH THE SAMPLER IMPLEMENTATIONS ###
+source("https://raw.githubusercontent.com/gzanella/TGS/master/functions_for_BVS.R")
 
 ###### SPECIFYING THE MODEL AND SIMULATING DATA #######
 ####
-n<-100
-p<-100
-c<-10^3
-prior_p_incl<-5/p
-hyper_par<-simulate_data(n=n,p=p,c=c,SNR=3,scenario=1)
+n<-100 # n.observations
+p<-100 # n.covariates
+c<-10^3 #prior hyperparameter for the covariance matrix
+prior_p_incl<-5/p #prior prob of inclusion
+hyper_par<-simulate_data(n=n,p=p,c=c,SNR=3,scenario=1) # creates list to be passed to samplers
 
 ###### RUNNING THE SAMPLERS #######
 ####
-T<-50000
+T<-50000 #n.iterations
 burn_in<-5000
-output_TGS<-TGS(p=p,hyper_par=hyper_par,T=T,burn_in=burn_in)
-thin_GS<-5
+thin_GS<-2
 output_GS<-GS(p=p,hyper_par=hyper_par,T=T,burn_in=burn_in,thin = thin_GS)
+output_TGS<-TGS(p=p,hyper_par=hyper_par,T=T,burn_in=burn_in)
 output_wTGS<-wTGS(p=p,hyper_par=hyper_par,T=T,burn_in=burn_in)
 
 
@@ -24,14 +24,14 @@ output_wTGS<-wTGS(p=p,hyper_par=hyper_par,T=T,burn_in=burn_in)
 
 par(mfrow=c(1,2))
 col_GS<-"black";col_TGS<-"blue";col_wTGS<-"red"
-# plot running estimates of Pr(gamma_1=1|Y) for different samplers
+# PLOT RUNNING ESTIMATES OF Pr(gamma_1=1|Y) FOR DIFFERENT SAMPLERS
 plot(output_TGS$est_inclusion_probs_1/cumsum(output_TGS$sample_weights),
      ylim=c(0,1),type="l",col=col_TGS,xlab="Sampler iteration", ylab="Posterior inclusion prob.")
 lines(cumsum(output_GS$gamma_1[(burn_in+1):(burn_in+T)])/c(1:T),
       col=col_GS)
 lines(output_wTGS$est_inclusion_probs_1/cumsum(output_wTGS$sample_weights),type="l",col=col_wTGS)
 title("Variable 1")
-# plot running estimates of Pr(gamma_2=1|Y) for different samplers
+# PLOT RUNNING ESTIMATES OF Pr(gamma_2=1|Y) FOR DIFFERENT SAMPLERS
 plot(output_TGS$est_inclusion_probs_2/cumsum(output_TGS$sample_weights),
      ylim=c(0,1),type="l",col=col_TGS,xlab="Sampler iteration", ylab="Posterior inclusion prob.")
 lines(cumsum(output_GS$gamma_2[(burn_in+1):(burn_in+T)])/c(1:T),col=col_GS)
